f69b7bf537b2f73c0a970a9ddf5463e338b437a9
Pratyush Biswas <pratyush.biswas1@gmail.com>
2026-04-26 11:39:59 +0530

    Update copyright

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -26,7 +26,7 @@ const Footer = () => {
 
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
         <p className="md:text-base text-sm md:font-normal font-light">
-          Copyright © 2025 Pratyush Biswas
+          Copyright © 2026 Pratyush Biswas
         </p>
 
         <div className="flex items-center md:gap-3 gap-2 mt-6 md:mt-0">
